13713045cf74be3159a22aaaaf108a9e5cfc83b2
Giulian Gonçalves Vivan <giulian@siliconant.com>
2020-02-06 16:45:20 -0300

    slamr adjustments for django

--- siliconant/settings.py
+++ siliconant/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'siliconant.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,4 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/' # used in development
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # used in production
+
+LOGOUT_REDIRECT_URL = 'index'
